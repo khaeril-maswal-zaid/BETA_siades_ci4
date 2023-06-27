7dfb24a7c837bac453bb6b593ca50466fe422c42
Khaeril Maswal Zaid <muhammadkhaerilzaid@gmail.com>
2023-06-27 11:45:26 +0800

    update admin: data desa

--- app/Views/admin/data-desa.php
+++ app/Views/admin/data-desa.php
@@ -7,7 +7,10 @@
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
             <h1 class="h3"><?= $label ?></h1>
 
-            <a href="#" class="btn btn-sm btn-success">Tambah Data</a>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Tambah Data
+            </button>
 
         </div>
     </div>
@@ -64,5 +67,24 @@
         <div id="container"></div>
     </figure>
 </main>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php $this->endSection() ?>
